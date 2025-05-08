ba445649553602d76f9be8eb6dc77b2b9e55a986
NgoMinhAn <ngominhan666@gmail.com>
2025-05-08 10:00:13 +0700

    About us page fix 2

--- app/controllers/PageController.php
+++ app/controllers/PageController.php
@@ -30,6 +30,50 @@ class PageController {
     }
     
     /**
+     * Contact method - displays the contact page
+     */
+    public function contact() {
+        $data = [
+            'title' => 'Contact Us'
+        ];
+        
+        $this->loadView('pages/contact', $data);
+    }
+    
+    /**
+     * Terms method - displays the terms of service
+     */
+    public function terms() {
+        $data = [
+            'title' => 'Terms of Service'
+        ];
+        
+        $this->loadView('pages/terms', $data);
+    }
+    
+    /**
+     * Privacy method - displays the privacy policy
+     */
+    public function privacy() {
+        $data = [
+            'title' => 'Privacy Policy'
+        ];
+        
+        $this->loadView('pages/privacy', $data);
+    }
+    
+    /**
+     * Careers method - displays the careers page
+     */
+    public function careers() {
+        $data = [
+            'title' => 'Careers'
+        ];
+        
+        $this->loadView('pages/careers', $data);
+    }
+    
+    /**
      * Load view helper
      * 
      * @param string $view The view file
