8f5e37f068fea41d7218a9fe403cae2f2255d021
ptt-04 <phantritai1552004@gmail.com>
2025-11-30 20:32:53 +0700

    1

--- tourguide/app/config/php-config.php
+++ tourguide/app/config/php-config.php
@@ -1,5 +1,7 @@
 <?php
-// Set the path to the SSL certificate
+// Set the path to the SSL certificate (only if file exists)
 $certPath = __DIR__ . '/../../cacert.pem';
-ini_set('curl.cainfo', $certPath);
-ini_set('openssl.cafile', $certPath); 
+if (file_exists($certPath)) {
+    ini_set('curl.cainfo', $certPath);
+    ini_set('openssl.cafile', $certPath);
+} 
